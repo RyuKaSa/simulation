86de3b4d98e4b48204dc4c1bb6f88b78c2a3dd2d
RyuKaSa <92441491+RyuKaSa@users.noreply.github.com>
2025-03-24 13:46:47 +0100

    template 2, city center

--- SoA/src/BlockWorld.cpp
+++ SoA/src/BlockWorld.cpp
@@ -63,7 +63,7 @@ bool BlockWorld::addBlock(int cx, int cy, int cz)
     m_blockIndex[coord] = newIndex;
 
     // Optionally print the updated list of blocks
-    // printBlockList();
+    printBlockList();
 
     return true;
 }
